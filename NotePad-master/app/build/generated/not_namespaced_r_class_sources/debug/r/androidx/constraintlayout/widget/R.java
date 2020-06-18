6b3e5c0316ec760decd64e26f041e1e6a930a96f
SayHi-error404 <1148575056@qq.com>
2020-06-18 11:51:33 +0800

    backup and restroe

--- NotePad-master/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ NotePad-master/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f070041;
-        public static final int end = 0x7f07005a;
-        public static final int gone = 0x7f07005f;
-        public static final int invisible = 0x7f070069;
-        public static final int left = 0x7f07006c;
-        public static final int packed = 0x7f070084;
-        public static final int parent = 0x7f070085;
-        public static final int percent = 0x7f070087;
-        public static final int right = 0x7f07008c;
-        public static final int spread = 0x7f0700a7;
-        public static final int spread_inside = 0x7f0700a8;
-        public static final int start = 0x7f0700ad;
-        public static final int top = 0x7f0700c3;
-        public static final int wrap = 0x7f0700ca;
+        public static final int bottom = 0x7f070043;
+        public static final int end = 0x7f07005c;
+        public static final int gone = 0x7f070061;
+        public static final int invisible = 0x7f07006b;
+        public static final int left = 0x7f07006e;
+        public static final int packed = 0x7f070086;
+        public static final int parent = 0x7f070087;
+        public static final int percent = 0x7f070089;
+        public static final int right = 0x7f070090;
+        public static final int spread = 0x7f0700ab;
+        public static final int spread_inside = 0x7f0700ac;
+        public static final int start = 0x7f0700b1;
+        public static final int top = 0x7f0700c7;
+        public static final int wrap = 0x7f0700ce;
     }
     public static final class styleable {
         private styleable() {}
--- NotePad-master/app/src/main/java/com/example/android/notepad/BlankFragment3.java
+++ NotePad-master/app/src/main/java/com/example/android/notepad/BlankFragment3.java
@@ -1,25 +1,30 @@
 package com.example.android.notepad;
 
-
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
+
+import android.widget.Toast;
+
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link BlankFragment#newInstance} factory method to
  * create an instance of this fragment.
+ * 对应着底部栏第三个界面,实现备份
  */
 public class BlankFragment3 extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_SHOW_TEXT = "text";
 
     private String mContentText;
-
 
     public BlankFragment3() {
         // Required empty public constructor
@@ -53,9 +58,85 @@ public class BlankFragment3 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_blank3, container, false);
-        TextView contentTv = rootView.findViewById(R.id.content_tv);
-        contentTv.setText(mContentText);
+        ImageButton backupButton = rootView.findViewById(R.id.backupButton);
+        ImageButton restoreButton = rootView.findViewById(R.id.restoreButton);
+
+        //设置点击后记录日志和Toast提示
+        backupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                createBackupDialog();
+                Log.i("blank3", "backupButton onClick");
+                //Toast.makeText(getActivity(),"备份",Toast.LENGTH_LONG).show();
+            }
+        });
+        restoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                createRestoreDialog();
+                Log.i("blank3", "restoreButton onClick");
+                //Toast.makeText(getActivity(),"还原",Toast.LENGTH_LONG).show();
+            }
+        });
+
         return rootView;
     }
+
+
+    public void createBackupDialog() {
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(getActivity());
+
+        builder.setTitle("您确定要备份嘛？");
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // 确定备份
+                Toast.makeText(getActivity(),"确定",Toast.LENGTH_LONG).show();
+
+                BackupTask backupTask = new BackupTask(getActivity());
+                String backup_version = backupTask.doInBackground("backupDatabase");
+                Toast.makeText(getActivity(),"备份时间："+backup_version,Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(getActivity(),"取消",Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.create();//创建对话框
+        builder.show();//显示对话框
+    }
+
+
+    public void createRestoreDialog() {
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(getActivity());
+
+        builder.setTitle("您确定要还原嘛？");
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // 确定备份
+                Toast.makeText(getActivity(),"确定",Toast.LENGTH_LONG).show();
+
+                BackupTask backupTask = new BackupTask(getActivity());
+                String backup_version = backupTask.doInBackground("restoreDatabase");
+                Toast.makeText(getActivity(),"上次备份时间："+backup_version,Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(getActivity(), BottomActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(getActivity(),"取消",Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.create();//创建对话框
+        builder.show();//显示对话框
+    }
+
 
 }
